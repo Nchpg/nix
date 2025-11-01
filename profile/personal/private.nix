f3a7104262fd90f343dbf4c2a21dcd317aa041c3
nathan.champagne <nathan.champagne@epita.fr>
2025-11-01 01:04:03 +0100

    modify git module

---
+++ profile/personal/private.nix
@@ -0,0 +1,11 @@
+# This is a template file for personal home.nix settings.
+{
+  config = {
+    userSettings = {
+      git = {
+        userName = "xavier.login"; 
+        userEmail = "xavier.login@example.com";
+      };
+    };
+  };
+}
