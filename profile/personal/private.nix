09ca458611b9306ee070e2fd9dabee1fb9f15282
nathan.champagne <nathan.champagne@epita.fr>
2025-11-01 18:33:47 +0100

    fix personal.nix

--- profile/personal/private.nix
+++ profile/personal/private.nix
@@ -1,10 +1,15 @@
 # This is a template file for personal home.nix settings.
+# Fill in your personal information below.
+
+# I recommend removing this file from version control with :
+# git update-index --assume-unchanged **/*/private.nix
+# So your personal information does not end up on github.
 {
   config = {
     userSettings = {
       git = {
-        userName = "xavier.login"; 
-        userEmail = "xavier.login@example.com";
+        #userName = "<your git user name>";
+        #userEmail = "<your git email address>";
       };
     };
   };
